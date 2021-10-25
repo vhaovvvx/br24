d8c7ec59888848397a5d8c36f6dde74bff9cf7c9
diego <doandtvp@gmail.com>
2021-10-25 11:24:38 +0700

    custom reuse button for project

---
+++ src/UI/Button/Button.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Button.css';
+
+function Button(props) {
+
+  const { bgc, mw, display, order, hanldeClick, disabled }  = props
+
+  const activeStyle = {
+    backgroundColor: bgc,
+    maxWidth: mw,
+    display: 'flex'
+  }
+
+  const icons = {
+    display: display,
+    order: order
+  }
+
+  const disabledStyle = {
+    maxWidth: mw,
+    backgroundColor: "#F4F5F6",
+    cursor: "not-allowed",
+    color: "DBE0E6",
+    boxShadow: "none"
+  }
+
+return (
+    <div className='buttons'>
+      <button 
+        className='button-group' 
+        style={disabled ? disabledStyle : activeStyle}
+        onClick={hanldeClick}
+        >
+          <div className='icons' style={icons}>
+            <div className='icons-content'></div>
+          </div>
+          <span>BUTTON</span>
+      </button>
+    </div>
+  )
+}
+
+export default Button
---
+++ src/UI/Button/OutLineButton.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './OutLineButton.css';
+
+function OutLineButton(props) {
+
+  const { mw, display, order, hanldeClick, disabled }  = props
+
+  const activeStyle = {
+    maxWidth: mw
+  }
+
+  const icons = {
+    display: display,
+    order: order
+  }
+
+  const disabledStyle = {
+    maxWidth: mw,
+    border: "1px solid #DBE0E6",
+    cursor: "not-allowed",
+    color: "DBE0E6",
+    boxShadow: "none"
+  }
+
+return (
+  <div className='ol-buttons'>
+    <button 
+      className='ol-button-group' 
+      style={disabled ? disabledStyle : activeStyle}
+      onClick={hanldeClick}
+      >
+      <div style={icons} className='ol-icons-content'></div>
+      <span>BUTTON</span>
+    </button>
+  </div>
+  )
+}
+
+export default OutLineButton
